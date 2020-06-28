c4e85fdd2f8f8986063e1bacb524e82677f2e4a6
Alexander Ignition <izh.sever@gmail.com>
2020-06-28 09:12:12 +0300

    Update BookStorage

--- Sources/Books/BookStorage.swift
+++ Sources/Books/BookStorage.swift
@@ -8,21 +8,12 @@ final class BookStorage {
     init(backgroundContex: NSManagedObjectContext) {
         self.backgroundContex = backgroundContex
     }
-
-    func rename(_ name: String, book: Book) -> AnyCancellable {
-        return URLSession.shared
-            .dataTaskPublisher(for: URL(string: "https://api.example")!)
-            .map(\.data)
-            .map { String(data: $0, encoding: .utf8) }
-            .replaceError(with: book.name)
-            .receive(on: book.managedObjectContext!)
-            .assign(to: \.name, on: book)
-    }
 }
 
 // MARK: - Save books
 
 extension BookStorage {
+
     func saveBooks(names: [String], completion: @escaping (Error?) -> Void) {
         backgroundContex.perform {
             for name in names {
@@ -37,9 +28,7 @@ extension BookStorage {
             }
         }
     }
-}
 
-extension BookStorage {
     func saveBooks(names: [String]) -> AnyPublisher<Void, Error> {
         backgroundContex.publisher {
             for name in names {
@@ -54,6 +43,7 @@ extension BookStorage {
 // MARK: - Fetch books
 
 extension BookStorage {
+
     func fetchBooks(completion: @escaping (Result<[Book], Error>) -> Void) {
         backgroundContex.perform {
             do {
@@ -64,9 +54,7 @@ extension BookStorage {
             }
         }
     }
-}
 
-extension BookStorage {
     func fetchBooks() -> AnyPublisher<[Book], Error> {
         backgroundContex.fetchPublisher(Book.all).eraseToAnyPublisher()
     }
